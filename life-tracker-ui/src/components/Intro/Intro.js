129fdde0d618f5f28b0eb1ca06614de09b784b46
elefe <eleferrets@gmail.com>
2021-06-25 18:35:44 -0400

    Added buttons under Resources

--- life-tracker-ui/src/components/Intro/Intro.js
+++ life-tracker-ui/src/components/Intro/Intro.js
@@ -9,6 +9,7 @@ export default function Intro() {
       <h2>Helping you towards a better you one step at a time</h2></div>
       
       <div className="image"><img src={fitness} alt="Fitness"></img></div>
+      
     </div> 
   );
 }
--- life-tracker-ui/src/components/StoreActivity/StoreActivity.js
+++ life-tracker-ui/src/components/StoreActivity/StoreActivity.js
@@ -1,12 +1,39 @@
 import { formatDate, formatAmount } from "../../utils/format";
 import "./StoreActivity.css";
 import { Link } from "react-router-dom";
+import fitness from "../../assets/fitness.jpg"
 
 export default function StoreActivity({ products = [] }) {
   return (
     <div className="StoreActivity">
-       <h2>Activities</h2>
-       <div className=""></div>
+       <h2>Resources</h2>
+       <div className="Categories">
+       <div>
+         <h3>Activity</h3>
+       <Link to="/activity">
+       <div className="image">
+         <button id="close-image"><img src={fitness} alt="Fitness"></img></button></div>
+       </Link>
+       </div>
+       <div>
+       <h3>Exercise</h3>
+       <Link to="/exercise">
+       <div className="image"><button id="close-image"><img src={fitness} alt="Fitness"></img></button></div>
+       </Link>
+       </div>
+       <div>
+       <h3>Nutrition</h3>
+       <Link to="/nutrition">
+       <div className="image"><button id="close-image"><img src={fitness} alt="Fitness"></img></button></div>
+       </Link>
+       </div>
+       <div>
+       <h3>Sleep</h3>
+       <Link to="/sleep">
+       <div className="image"><button id="close-image"><img src={fitness} alt="Fitness"></img></button></div>
+       </Link>
+       </div>
+       </div>
      
     </div>
   );
